feb9ffc2ea918513e279aa01cfe999c6629af3af
namean <dwilsoncnc@gmail.com>
2023-10-23 21:10:26 -0400

    able to send POST request to server, via cors

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ var http = require("http");
 var https = require("https");
 app.use(express.static("public"));
 
+const logger = {
+  get: () => {
+    console.log(`GET request: ${new Date().toLocaleTimeString()}`);
+  },
+  post: () => {
+    console.log(`[POST] received!: ${new Date().toLocaleTimeString()}`);
+  },
+};
+
 app.get("/", cors(), (req, res, next) => {
-  console.log(`GET request: ${new Date().toLocaleTimeString()}`);
+  logger.get();
   //   res.send("Hello World!");
   res.sendFile("http://localhost:3000/index.html");
   next();
+});
+
+app.post("/query", cors(), (req, res, next) => {
+  logger.post();
+  res.send({ msg: "POST: /query -> message received! " });
 });
 
 app.listen(port, () => {
